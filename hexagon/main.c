5a0b5be88c5319d7aca758453e6f0e54774b04bd
bademasta <badb@purg.at>
2025-10-18 00:10:59 +0200

    first commit hexagon

---
+++ hexagon/main.c
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define HEIGHT 27
+#define WIDTH 100
+#define mpx =  55
+#define mpy = 14
+
+char feld[HEIGHT][WIDTH];
+
+
+
+
+
+
+int main() {
+#pragma region feld füllen
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            feld[i][j] = '*';
+        }
+    }
+#pragma endregion feld füllen
+
+#pragma region mittelpunk
+feld[mpx][mpy] = ' ';
+#pragma endregion mittelpunk
+
+#pragma region feld ausgeben
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c", feld[i][j]);
+        }
+        printf("\n");
+    }
+#pragma endregion feld ausgeben
+
+}
